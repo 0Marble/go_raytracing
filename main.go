525664aadf15f046195743bff380afadf1f19cfa
Aleksei Lobanov <zeromarblectm@gmail.com>
2024-05-21 20:21:36 +0800

    parallel rendering of tiles

--- main.go
+++ main.go
@@ -9,14 +9,54 @@ import (
 	"raytracing/camera/postprocess"
 	"raytracing/camera/sensors"
 	scenes "raytracing/example_scenes"
+	"raytracing/materials"
 	"raytracing/raytracing"
 	"raytracing/scene"
 )
 
 func RenderAndSave(s scene.Scene, cam camera.Camera, fileName string) {
-	log.Println("Saving to ", fileName)
+	w := 500
+	h := 500
+	n := 10
+	tileWidth := w / n
+	tileHeight := h / n
+	tiles := make([]chan camera.Image, n*n)
+	img := camera.Image{Width: w, Height: h, Pixels: make([]materials.Color, w*h)}
+
+	log.Println("Rendering ", fileName)
 	rm := raytracing.InitSimpleRaytracer(s, 4)
-	img := cam.Shoot(&rm, 500, 500, 0, 500, 0, 500)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			c := make(chan camera.Image)
+			left := i * tileWidth
+			right := (i + 1) * tileWidth
+			bottom := j * tileHeight
+			top := (j + 1) * tileHeight
+			go func() {
+				img := cam.Shoot(&rm, w, h, left, right, bottom, top)
+				c <- img
+			}()
+			tiles[i*n+j] = c
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			c := tiles[i*n+j]
+			tile := <-c
+
+			left := i * tileWidth
+			right := (i + 1) * tileWidth
+			bottom := j * tileHeight
+			top := (j + 1) * tileHeight
+			for y := bottom; y < top; y++ {
+				for x := left; x < right; x++ {
+					img.Pixels[y*w+x] = tile.Pixels[(y-bottom)*tileWidth+(x-left)]
+				}
+			}
+		}
+	}
+
+	log.Println("Saving to ", fileName)
 
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -35,6 +75,6 @@ func main() {
 	s, camTransform := scenes.CornellScene()
 	camLens := lenses.InitProjectiveLens(camTransform)
 	pp := postprocess.InitGammaCorrection(1.0/2.2, &postprocess.NoProcessing{})
-	sensor := sensors.InitFsaaRgssSensor()
+	sensor := sensors.InitFsaaCheckerboardSensor(4)
 	RenderAndSave(s, camera.InitCamera(&camLens, &sensor, &pp), "images/img.png")
 }
